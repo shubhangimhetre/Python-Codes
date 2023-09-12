79e4bf3b3970cc3c569f5627058b6c7b7663d18b
Shubhangi Vishwanath Mhetre <89292248+shubhangivm@users.noreply.github.com>
2023-09-12 11:19:25 +0530

    Update Merakiq9.py

--- Dictionary-main/Merakiq9.py
+++ Dictionary-main/Merakiq9.py
@@ -4,15 +4,15 @@
 # # {'M':1,'I':4,'S':4,'P':2}
 # # str="MISSISSIPPI"
 
-# str=list("MISSISSIPPI")
-# list1=[]
-# dic={}
-# for i in str:
-#     if i not in dic:
-#         list1.append(i)
-#         c=0
-#         for j in str:
-#             if i == j:
-#                c=c+1
-#         dic[i]=c
-# print(dic)
+str=list("MISSISSIPPI")
+list1=[]
+dic={}
+for i in str:
+    if i not in dic:
+        list1.append(i)
+        c=0
+        for j in str:
+            if i == j:
+               c=c+1
+        dic[i]=c
+print(dic)
