59de204e737d07159618bc879a42e337a3d05351
Shubhangi Vishwanath Mhetre <89292248+shubhangivm@users.noreply.github.com>
2023-09-12 11:59:51 +0530

    Update Exponential.py

--- Hackerrank-main/Exponential.py
+++ Hackerrank-main/Exponential.py
@@ -1,10 +1,10 @@
 ##a**b+c**d
 
-# a=int(input("enter number"))
-# b=int(input("enter number"))
-# c=int(input("enter number"))
-# d=int(input("enter number"))
-# k=a**b
-# l=c**d
-# m=k+l
-# print(m)
+a=int(input("enter number"))
+b=int(input("enter number"))
+c=int(input("enter number"))
+d=int(input("enter number"))
+k=a**b
+l=c**d
+m=k+l
+print(m)
